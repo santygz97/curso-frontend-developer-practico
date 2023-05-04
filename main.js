0f9da00db871d786e7561c0e4691fe292ed28bea
Santiago <santiago.galea97@gmail.com>
2023-05-03 23:49:41 -0500

    Fusion de clases 8 y 13 al index

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shopingCartMenu = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobileMenu);
+shopingCartMenu.addEventListener('click', toggleProductDetailMenu);
 
 
 function toggleDesktopMenu() {
@@ -14,4 +20,14 @@ function toggleDesktopMenu() {
 
     //Solucion del profesor
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    aside.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleProductDetailMenu() {
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
